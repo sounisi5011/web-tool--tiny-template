e4de95031810aa3b820471a229ca9ee80e4282f4
Sonishi Izuka <sounisi5011@jedofuka.local>
2021-03-26 01:10:48 +0900

    IMEの入力を確定するEnterキー押下を除外する

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,5 +1,6 @@
 export function triggerEnter<T extends KeyboardEvent>(fn: (event: T) => void): (event: T) => void {
     return event => {
-        if (event.key === 'Enter') fn(event);
+        // Note: IMEの入力確定のEnter入力を除外するため、isComposingプロパティがfalseであることを確認する
+        if (event.key === 'Enter' && !event.isComposing) fn(event);
     };
 }
